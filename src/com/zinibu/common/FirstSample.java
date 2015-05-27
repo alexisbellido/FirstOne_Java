7b2d1ab367e517c63ca15a35dbe37dd67036cdc5
Alexis Bellido <alexis@ventanazul.com>
2015-05-27 12:13:36 -0400

    foreach loop

--- src/com/zinibu/common/FirstSample.java
+++ src/com/zinibu/common/FirstSample.java
@@ -24,6 +24,22 @@ public class FirstSample {
 
         // Now, let's test arrays.
 
+        int[] a = new int[10];
+        a[0] = 1;
+        a[1] = 2;
+        for (int i = 0; i < a.length; i++) {
+            a[i] = i;
+        }
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.printf("a[%d] = %d\n", i, a[i]);
+        }
+
+        for (int element: a) {
+            System.out.printf("number: %d\n", element);
+        }
+
+
     }
 
 }
