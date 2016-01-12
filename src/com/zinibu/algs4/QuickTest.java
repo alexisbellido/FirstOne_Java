04aea3aa519234f7903ee76b505d22f865945307
Alexis Bellido <alexis@ventanazul.com>
2016-01-11 20:17:06 -0500

    QuickTest

---
+++ src/com/zinibu/algs4/QuickTest.java
@@ -0,0 +1,10 @@
+package com.zinibu.algs4;
+
+/**
+ * Created by alexis on 1/11/16.
+ */
+public class QuickTest {
+    public static void main(String[] args) {
+        System.out.println("Testing");
+    }
+}
